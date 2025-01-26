a1bfdb7b358179cc6fc430a86f3233493a7e1f2d
msk <msky0912@gmail.com>
2025-01-26 11:58:35 +0900

    직접 만든 세션을 적용하여 로그인 처리
     - 로그인 성공시 세션 등록하고, 세션에 loginMember를 저장해두고, 쿠키도 함께 발행
     - 로그아웃시 해당 세션의 정보를 제거
     - homeLogin()에서 세션관리자에 저장된 회원 정보를 조회하고, 회원정보가 없으면 쿠키나 세션이 없는 것이므로 비로그인 처리

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,20 +2,25 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @Controller
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
-    public HomeController(MemberRepository memberRepository) {
+    public HomeController(MemberRepository memberRepository, SessionManager sessionManager) {
         this.memberRepository = memberRepository;
+        this.sessionManager = sessionManager;
     }
 
 //    @GetMapping("/")
@@ -23,7 +28,7 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if (memberId == null) {
             return "home";
@@ -36,6 +41,21 @@ public class HomeController {
         }
 
         model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        //세션 관리자에 저장된 회원 정보 조회
+        //회원정보가 없으면, 쿠키나 세션이 없는 것이므로 로그인되지 않은 것으로 처리
+        Member member = (Member) sessionManager.getSession(request);
+        if (member == null) {
+            return "home";
+        }
+
+        //로그인
+        model.addAttribute("member", member);
         return "loginHome";
     }
 }
